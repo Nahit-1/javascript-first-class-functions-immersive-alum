bc7c5c454fdb7547757ea4e960dcd64521a746f6
Nahit-1 <nahit86@outlook.com>
2019-10-24 18:58:55 +0100

    done

--- index.js
+++ index.js
@@ -1,68 +1,20 @@
-function runFiveMiles() {
-    console.log('Go for a five-mile run');
-  }
-  
-  function liftWeights() {
-    console.log('Pump iron');
-  }
-  
-  function swimFortyLaps() {
-    console.log('Swim 40 laps');
-  }
-  
-  function exerciseRoutine(postRunActivity) {
-    runFiveMiles();
-    postRunActivity();
-  }
-  
-  function Monday() {
-    exerciseRoutine(liftWeights);
-  }
-  
-  // Pilates time!
-  function Tuesday() {
-    exerciseRoutine(function() {
-      console.log('Stretch! Work that core!');
-    });
-  
-    // OR:
-    exerciseRoutine(() => {
-      console.log('Stretch! Work that core!');
-    });
-  
-    // OR:
-    exerciseRoutine(() => console.log('Stretch! Work that core!'));
-  }
-  
-  function Wednesday() {
-    exerciseRoutine(runFiveMiles);
-  }
-  
-  function Thursday() {
-    exerciseRoutine(liftWeights);
-  }
-  
-  function Friday() {
-    exerciseRoutine(swimFortyLaps);
-  }
-  
-  function morningRoutine(exercise) {
-    var breakfast = null;
-  
-    if (exercise === liftWeights) {
-      breakfast = 'protein bar';
-    } else if (exercise === swimFortyLaps) {
-      breakfast = 'kale smoothie';
-    } else {
-      breakfast = 'granola';
-    }
-  
-    exerciseRoutine(exercise);
-  
-    return function() {
-      console.log(`Nom nom nom, this ${breakfast} is delicious!`);
-    };
-  }
-  
-  var afterExercise = morningRoutine(liftWeights);
-  afterExercise();
+function countdown(callback) {
+  setTimeout(function () {
+    callback();
+  }, 2000);
+}
+
+function createMultiplier(multiplyValue) {
+  return function (value) {
+    return multiplyValue * value;
+  };
+}
+
+function multiplier(a, b) {
+  return a * b;
+}
+
+var doubler = createMultiplier(2);
+var tripler = createMultiplier(3);
+var doublerWithBind = multiplier.bind(null, 2);
+var triplerWithBind = multiplier.bind(null, 3);
